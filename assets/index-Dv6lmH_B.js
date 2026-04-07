6ee226ead34abe31a1f620e689df71f5b8605c8f
AdhesiveEarth1 <AdhesiveEarth1@users.noreply.github.com>
2026-04-07 16:45:58 +0000

    deploy: 967818559ee6565174f4364f73d7fb937bbe9d96

--- assets/index-Dv6lmH_B.js
+++ assets/index-Dv6lmH_B.js
@@ -1,9 +1,9 @@
-(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const a of e)if(a.type==="childList")for(const s of a.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&o(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const a={};return e.integrity&&(a.integrity=e.integrity),e.referrerPolicy&&(a.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?a.credentials="include":e.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function o(e){if(e.ep)return;e.ep=!0;const a=n(e);fetch(e.href,a)}})();const d="/CUOpenDayTest/vite.svg",f="/CUOpenDayTest/tailwindcss-mark.svg",p="/CUOpenDayTest/typescript.svg",r="/CUOpenDayTest/cu-logo.svg";async function m(){return await(await fetch("/CUOpenDayTest/api/OpenDay.json")).json()}function u(i,t){return i.topics.filter(o=>o.name&&o.name.charAt(0).toUpperCase()===t.toUpperCase())}function l(){return`
+(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))o(e);new MutationObserver(e=>{for(const a of e)if(a.type==="childList")for(const s of a.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&o(s)}).observe(document,{childList:!0,subtree:!0});function n(e){const a={};return e.integrity&&(a.integrity=e.integrity),e.referrerPolicy&&(a.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?a.credentials="include":e.crossOrigin==="anonymous"?a.credentials="omit":a.credentials="same-origin",a}function o(e){if(e.ep)return;e.ep=!0;const a=n(e);fetch(e.href,a)}})();const c="/CUOpenDayTest/vite.svg",f="/CUOpenDayTest/tailwindcss-mark.svg",p="/CUOpenDayTest/typescript.svg",l="/CUOpenDayTest/cu-logo.svg";async function u(){return await(await fetch("/CUOpenDayTest/api/OpenDay.json")).json()}function m(i,t){return i.topics.filter(o=>o.name&&o.name.charAt(0).toUpperCase()===t.toUpperCase())}function r(){return`
     <div class="demo-banner w-full bg-yellow-300 text-black flex flex-col sm:flex-row items-center justify-between px-4 py-2 mb-6 gap-2 border-b-2 border-yellow-500">
       <div class="font-bold text-lg flex-1 text-center sm:text-left">This is a demo app</div>
       <div class="flex flex-row items-center gap-3 justify-center">
         <a href="https://vitejs.dev/" target="_blank" rel="noopener noreferrer">
-          <img src="${d}" alt="Vite Logo" class="h-8 w-auto" />
+          <img src="${c}" alt="Vite Logo" class="h-8 w-auto" />
         </a>
         <a href="https://tailwindcss.com/" target="_blank" rel="noopener noreferrer">
           <img src="${f}" alt="Tailwind CSS Logo" class="h-8 w-auto" />
@@ -13,7 +13,7 @@
         </a>
       </div>
     </div>
-  `}function v(){return`
+  `}function g(){return`
     <div class="filter-container mb-6 py-6">
     <div class = "text-cardiff-dark">
     <p>Select Starting Letter to Filter By</p>
@@ -50,22 +50,31 @@
       <button id="filterButton">Filter</button>
       </div>
     </div>
-  `}function g(){return`
-    <div class="filter-container mb-6">
+  `}function h(){return`
+    <div class="filter-container mb-6 py-6">
       <button id="backButton">Back</button>
     </div>
-  `}function c(i){return`
+  `}function d(i){return` 
     <div class="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-2">
       ${i.map((t,n)=>{const o=`dialog-${n}`;return`
+
+        <!-- Below is the code that brings up the main picture, title, description and button for each subject card-->
+
         <div class="bg-cardiff-red rounded-lg shadow p-6 flex flex-col">
-          <img src="${t.cover_image||r}" alt="${t.name}" class="h-32 w-full object-cover rounded mb-4" />
+          <img src="${t.cover_image||l}" alt="${t.name}" class="h-32 w-full object-cover rounded mb-4" />
           <h2 class="text-xl font-bold text-cardiff-white mb-2">${t.name}</h2>
           <p class="text-cardiff-white mb-2">${t.description||""}</p>
-          
-          <button command="show-modal" commandfor="${o}" class="rounded-md bg-cardiff-dark px-2.5 py-1.5 text-sm font-semibold text-cardiff-white ring-1 ring-inset ring-white/5">
+
+          <!-- Below, we ensure that we have the dialogId and display the button  -->
+          <button command="show-modal" commandfor="${o}" class="rounded-md bg-cardiff-dark px-2.5 py-1.5 text-sm font-semibold text-cardiff-white ring-1 ring-inset ring-cardiff-white/5">
             Show Information
           </button>
           
+        <!-- Below is the dialogue box for the info pop up, I installed the @tailwindplus/elements package to use this -->
+        <!-- We make a modal (pop up box) for each subject, then we use the dialogId variable stored within each card to show the correct corresponding modal-->
+
+        <!-- Below is where we make use of that dialogId again (as well as the index variable), this time to prepare the info pop up box. There is also a lot of CSS here for the box pop up, modified from the example code found here: https://tailwindcss.com/plus/ui-blocks/application-ui/overlays/modal-dialogs -->
+        <!-- Aside from the modification to make it a pop up box (and the occasional inclusion of an index and/or dialogId variable) the majority of the code is the same as what was given to me -->
           <el-dialog>
             <dialog id="${o}" aria-labelledby="dialog-title-${n}" class="fixed inset-0 m-0 size-auto max-h-none max-w-none overflow-y-auto bg-transparent p-0 backdrop:bg-transparent">
               <el-dialog-backdrop class="fixed inset-0 bg-cardiff-grey/50 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200"></el-dialog-backdrop>
@@ -80,9 +89,10 @@
                           <p class="text-sm text-cardiff-grey">
                             ${t.programs&&t.programs.length?`
                               <div class="mt-2">
-                                <h3 class="font-semibold text-cardiff-dark mb-1">Events:</h3>
+                                <h3 class="font-semibold text-cardiff-light mb-1">Events:</h3>
                                 <ul class="list-disc list-inside text-sm">
-                                  ${t.programs.map(e=>e&&e.title?`<li><span class="font-semibold">${e.title}</span>${e.start_time?` <span class='text-xs text-cardiff-red'>(${new Date(e.start_time).toLocaleTimeString([],{hour:"2-digit",minute:"2-digit"})}${e.end_time?" - "+new Date(e.end_time).toLocaleTimeString([],{hour:"2-digit",minute:"2-digit"}):""})</span>`:""}${e.room?`, <span class='text-xs'>${e.room}</span>`:""}${e.location.title?`, <span class='text-xs'>${e.location.title}</span>`:""}${e.location.address?`, <span class='text-xs'>${e.location.address}</span>`:""}${e.location.postcode?`, <span class='text-xs'>${e.location.postcode}</span>`:""}</li>`:"").join("")}
+                                <!-- In the line below, we have added the address of the buildings and a horizontal rule between each event -->
+                                  ${t.programs.map(e=>e&&e.title?`<li><span class="font-semibold">${e.title}</span>${e.start_time?` <span class='text-xs text-cardiff-red'>(${new Date(e.start_time).toLocaleTimeString([],{hour:"2-digit",minute:"2-digit"})}${e.end_time?" - "+new Date(e.end_time).toLocaleTimeString([],{hour:"2-digit",minute:"2-digit"}):""})</span>`:""}${e.room?`, <span class='text-xs'>${e.room}</span>`:""}${e.location.title?`, <span class='text-xs'>${e.location.title}</span>`:""}${e.location.address?`, <span class='text-xs'>${e.location.address}</span>`:""}${e.location.postcode?`, <span class='text-xs'>${e.location.postcode}</span><hr class = "bg-cardiff-light"> `:""}</li>`:"").join("")}
                                 </ul>
                               </div>
                             `:""}
@@ -92,7 +102,7 @@
                     </div>
                   </div>
                   <div class="bg-gray-700/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                    <button type="button" command="close" commandfor="${o}" class="inline-flex w-full justify-center rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-cardiff-white hover:bg-red-400 sm:ml-3 sm:w-auto">Close</button>
+                    <button type="button" command="close" commandfor="${o}" class="inline-flex w-full justify-center rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-cardiff-white hover:bg-cardiff-red sm:ml-3 sm:w-auto">Close</button>
                   </div>
                 </el-dialog-panel>
               </div>
@@ -101,14 +111,41 @@
         </div>
         `}).join("")}
     </div>
-  `}function x(i){const t=document.querySelector("#app");if(!i||!i.topics||i.topics.length===0){alert("No open day data matching your criteria found");return}t.innerHTML=`
-    ${l()}
+  `}function v(i){const t=document.querySelector("#app");if(!i||!i.topics||i.topics.length===0){t.innerHTML='<p class="text-red-600">No Open Day data found.</p>';return}t.innerHTML=`
+<!-- Below is an example of one of the function uses, it makes the webpage a little bit more modular -->
+    ${r()}
     
 
     <div class="min-h-screen bg-cardiff-white font-sans py-6">
       <div class="flex flex-col sm:flex-row items-center justify-center gap-4 mb-6">
         <a href="https://www.cardiff.ac.uk/" target="_blank" rel="noopener noreferrer">
-          <img src="${r}" alt="Cardiff University Logo" class="h-16 w-auto" />
+          <img src="${l}" alt="Cardiff University Logo" class="h-16 w-auto" />
+        </a>  
+      </div>
+      <a>
+      <!-- I took the liberty of taking one of the image links of the main building in one of the .json files given to me and linking it here, making it look more like a proper cardiff university webpage in my opinion -->
+        <img src="https://cardiff.imgix.net/__data/assets/image/0007/1380454/main-building-open-day.jpg?w=800%5Cu0026fit=crop%5Cu0026q=60%5Cu0026auto=format" alt="Cardiff University Main Building" class="h-32 w-full object-cover rounded mb-4">
+      </a>
+      <div class="bg-cardiff-red px-0 py-6">
+        <div class="customwidth">
+          <h1 class="text-3xl sm:text-5xl px-2 font-bold text-cardiff-white  text-left">Cardiff University Open Day</h1>
+        </div>
+      </div>
+        <div class="flex flex-col sm:flex-row items-center justify-center gap-4 mb-6">
+        
+       ${g()}
+       </div>
+      ${d(i.topics)}
+    </div>
+   
+  `,document.getElementById("filterButton")?.addEventListener("click",()=>{const n=document.getElementById("mySelect").value,o=m(i,n);b({topics:o})})}function b(i){const t=document.querySelector("#app");if(!i||!i.topics||i.topics.length===0){alert("No Subjects Matching Filter Criteria Found");return}t.innerHTML=`
+    ${r()}
+    
+
+    <div class="min-h-screen bg-cardiff-white font-sans py-6">
+      <div class="flex flex-col sm:flex-row items-center justify-center gap-4 mb-6">
+        <a href="https://www.cardiff.ac.uk/" target="_blank" rel="noopener noreferrer">
+          <img src="${l}" alt="Cardiff University Logo" class="h-16 w-auto" />
         </a>  
       </div>
       <a>
@@ -120,31 +157,9 @@
         </div>
       </div>
         <div class="flex flex-col sm:flex-row items-center justify-center gap-4 mb-6">
-       ${v()}
+       ${h()}
        </div>
-      ${c(i.topics)}
+      ${d(i.topics)}
     </div>
    
-  `,document.getElementById("filterButton")?.addEventListener("click",()=>{const n=document.getElementById("mySelect").value,o=u(i,n);b({topics:o})})}function b(i){const t=document.querySelector("#app");if(!i||!i.topics||i.topics.length===0){alert("No open day data matching your criteria found");return}t.innerHTML=`
-    ${l()}
-    
-    <div class="min-h-screen bg-cardiff-white font-sans py-6">
-      <div class="flex flex-col sm:flex-row items-center justify-center gap-4 mb-6">
-        <a href="https://www.cardiff.ac.uk/" target="_blank" rel="noopener noreferrer">
-          <img src="${r}" alt="Cardiff University Logo" class="h-16 w-auto" />
-        </a>  
-      </div>
-      <a>
-        <img src="https://cardiff.imgix.net/__data/assets/image/0007/1380454/main-building-open-day.jpg?w=800%5Cu0026fit=crop%5Cu0026q=60%5Cu0026auto=format" alt="Cardiff University Main Building" class="h-32 w-full object-cover rounded mb-4">
-      </a>
-      <div class="bg-cardiff-red px-0 py-6">
-        <div class="customwidth">
-          <h1 class="text-3xl sm:text-5xl px-2 font-bold text-cardiff-white  text-left">Cardiff University Open Day</h1>
-        </div>
-      </div>
-        <div class="flex flex-col sm:flex-row items-center justify-left gap-4 py-6 mb-6">
-       ${g()}
-       </div>
-      ${c(i.topics)}
-    </div>
-  `,document.getElementById("backButton")?.addEventListener("click",()=>{window.location.reload()})}m().then(x);
+  `,document.getElementById("backButton")?.addEventListener("click",()=>{window.location.reload()})}u().then(v);
