de2d5a99688232dfd501ba37c954936a6875b932
Ieuan Underdown <underdownir@cardiff.ac.uk>
2026-04-07 15:38:47 +0100

    Show Info Boxes Work

--- src/main.ts
+++ src/main.ts
@@ -99,36 +99,58 @@ function renderBackUI() {
 // Render the grid of topics
 function renderTopics(topics: any) {
   return `
-    <div class="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  px-2">
-      ${topics.map((topic: any) => `
+    <div class="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 px-2">
+      ${topics.map((topic: any, index: number) => {
+        const dialogId = `dialog-${index}`;
+        return `
         <div class="bg-cardiff-red rounded-lg shadow p-6 flex flex-col">
           <img src="${topic.cover_image || cuLogo}" alt="${topic.name}" class="h-32 w-full object-cover rounded mb-4" />
           <h2 class="text-xl font-bold text-cardiff-white mb-2">${topic.name}</h2>
           <p class="text-cardiff-white mb-2">${topic.description || ''}</p>
-          <button id="subjectButton" onclick= "sessionStorage.setItem('Subject', JSON.stringify([${topic.id}]))"> Show Event Info </button>
+          
+          <button command="show-modal" commandfor="${dialogId}" class="rounded-md bg-cardiff-dark px-2.5 py-1.5 text-sm font-semibold text-cardiff-white ring-1 ring-inset ring-white/5">
+            Show Information
+          </button>
+          
+          <el-dialog>
+            <dialog id="${dialogId}" aria-labelledby="dialog-title-${index}" class="fixed inset-0 m-0 size-auto max-h-none max-w-none overflow-y-auto bg-transparent p-0 backdrop:bg-transparent">
+              <el-dialog-backdrop class="fixed inset-0 bg-cardiff-grey/50 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200"></el-dialog-backdrop>
+              
+              <div tabindex="0" class="flex min-h-full items-end justify-center p-4 text-center focus:outline-none sm:items-center sm:p-0">
+                <el-dialog-panel class="relative transform overflow-hidden rounded-lg bg-cardiff-dark text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 sm:my-8 sm:w-full sm:max-w-lg">
+                  <div class="bg-cardiff-dark px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                    <div class="sm:flex sm:items-start">
+                      <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                        <h3 id="dialog-title-${index}" class="text-base font-semibold text-cardiff-white">Information</h3>
+                        <div class="mt-2">
+                          <p class="text-sm text-cardiff-grey">
+                            ${topic.programs && topic.programs.length ? `
+                              <div class="mt-2">
+                                <h3 class="font-semibold text-cardiff-dark mb-1">Events:</h3>
+                                <ul class="list-disc list-inside text-sm">
+                                  ${topic.programs.map((prog: any) => prog && prog.title ? `<li><span class="font-semibold">${prog.title}</span>${prog.start_time ? ` <span class='text-xs text-cardiff-red'>(${new Date(prog.start_time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}${prog.end_time ? ' - ' + new Date(prog.end_time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) : ''})</span>` : ''}${prog.room ? `, <span class='text-xs'>${prog.room}</span>` : ''}</li>` : '').join('')}
+                                </ul>
+                              </div>
+                            ` : ''}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="bg-gray-700/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                    <button type="button" command="close" commandfor="${dialogId}" class="inline-flex w-full justify-center rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-cardiff-white hover:bg-red-400 sm:ml-3 sm:w-auto">Close</button>
+                  </div>
+                </el-dialog-panel>
+              </div>
+            </dialog>
+          </el-dialog>
         </div>
-      `).join('')}
+        `;
+      }).join('')}
     </div>
   `;
 }
 
-function renderIndividualTopicInfo(topic: any){
-  topic = localStorage.getItem('Subject')
-  topic = JSON.stringify(topic)
-  alert(topic)
-  const app = document.querySelector<HTMLDivElement>('#app')!;
-  app.innerHTML = `
-  ${topic.programs && topic.programs.length ? `
-  <div class="mt-2">
-    <h3 class="font-semibold text-cardiff-dark mb-1">Events:</h3>
-    <ul class="list-disc list-inside text-sm">
-      ${topic.programs.map((prog: any) => prog && prog.title ? `<li><span class="font-semibold">${prog.title}</span>${prog.start_time ? ` <span class='text-xs text-cardiff-dark'>(${new Date(prog.start_time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})}${prog.end_time ? ' - ' + new Date(prog.end_time).toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'}) : ''})</span>` : ''}${prog.room ? `, <span class='text-xs'>${prog.room}</span>` : ''}</li>` : '').join('')}
-    </ul>
-  </div>
-` : ''}`
-
-
-}
 
 
 
@@ -145,50 +167,6 @@ function renderOpenDay(data: any) {
   app.innerHTML = `
     ${renderHeader()}
     
-
-<button command="show-modal" commandfor="dialog" class="rounded-md bg-white/10 px-2.5 py-1.5 text-sm font-semibold text-white ring-1 ring-inset ring-white/5 hover:bg-white/20">Open dialog</button>
-<el-dialog>
-  <dialog id="dialog" aria-labelledby="dialog-title" class="fixed inset-0 m-0 size-auto max-h-none max-w-none overflow-y-auto bg-transparent p-0 backdrop:bg-transparent">
-    <el-dialog-backdrop class="fixed inset-0 bg-gray-900/50 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"></el-dialog-backdrop>
-
-    <div tabindex="0" class="flex min-h-full items-end justify-center p-4 text-center focus:outline focus:outline-0 sm:items-center sm:p-0">
-      <el-dialog-panel class="relative transform overflow-hidden rounded-lg bg-gray-800 text-left shadow-xl outline outline-1 -outline-offset-1 outline-white/10 transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95">
-        <div class="bg-gray-800 px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-          <div class="sm:flex sm:items-start">
-            <div class="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-500/10 sm:mx-0 sm:size-10">
-              <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 text-red-400">
-                <path d="M12 9v3.75m-9.303 3.376c-.866 1.5.217 3.374 1.948 3.374h14.71c1.73 0 2.813-1.874 1.948-3.374L13.949 3.378c-.866-1.5-3.032-1.5-3.898 0L2.697 16.126ZM12 15.75h.007v.008H12v-.008Z" stroke-linecap="round" stroke-linejoin="round" />
-              </svg>
-            </div>
-            <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-              <h3 id="dialog-title" class="text-base font-semibold text-white">Deactivate account</h3>
-              <div class="mt-2">
-                <p class="text-sm text-gray-400">Are you sure you want to deactivate your account? All of your data will be permanently removed. This action cannot be undone.</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="bg-gray-700/25 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-          <button type="button" command="close" commandfor="dialog" class="inline-flex w-full justify-center rounded-md bg-red-500 px-3 py-2 text-sm font-semibold text-white hover:bg-red-400 sm:ml-3 sm:w-auto">Deactivate</button>
-          <button type="button" command="close" commandfor="dialog" class="mt-3 inline-flex w-full justify-center rounded-md bg-white/10 px-3 py-2 text-sm font-semibold text-white ring-1 ring-inset ring-white/5 hover:bg-white/20 sm:mt-0 sm:w-auto">Cancel</button>
-        </div>
-      </el-dialog-panel>
-    </div>
-  </dialog>
-</el-dialog>
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     <div class="min-h-screen bg-cardiff-white font-sans py-6">
       <div class="flex flex-col sm:flex-row items-center justify-center gap-4 mb-6">
@@ -222,12 +200,6 @@ function renderOpenDay(data: any) {
     
     renderFilterOpenDay({ topics: filteredData }); // Re-render with filtered data
   });
-
-  document.getElementById("subjectButton")?.addEventListener("click", () => {
-    alert(sessionStorage.getItem('Subject'))
-     renderIndividualTopicInfo
-  });
-
 
 
 }
